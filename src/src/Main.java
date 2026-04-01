3b904847afc7b7104b78df096afcdedf7c1fe7db
CodeDevAyush <ayushjoshi692@gmail.com>
2026-04-01 09:51:08 +0530

    UC2

--- src/src/Main.java
+++ src/src/Main.java
@@ -9,8 +9,29 @@ public class Main {
         System.out.println("=== Train Consist Management App ===");
 
         List<String> trainConsist = new ArrayList<>();
-
         System.out.println("Train consist initialized.");
         System.out.println("Initial bogie count: " + trainConsist.size());
+
+        // ===== UC2 =====
+        List<String> passengerBogies = new ArrayList<>();
+
+        passengerBogies.add("Sleeper");
+        passengerBogies.add("AC Chair");
+        passengerBogies.add("First Class");
+
+        System.out.println("\nPassenger Bogies after addition:");
+        System.out.println(passengerBogies);
+
+        passengerBogies.remove("AC Chair");
+
+        System.out.println("\nAfter removing AC Chair:");
+        System.out.println(passengerBogies);
+
+        if (passengerBogies.contains("Sleeper")) {
+            System.out.println("\nSleeper bogie exists in the train.");
+        }
+
+        System.out.println("\nFinal Passenger Bogie List:");
+        System.out.println(passengerBogies);
     }
 }
