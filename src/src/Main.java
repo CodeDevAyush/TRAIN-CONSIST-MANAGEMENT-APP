f92f0ea47e2488ed05e48e1de9a5d03585549b2a
CodeDevAyush <ayushjoshi692@gmail.com>
2026-04-01 09:54:14 +0530

    UC6

--- src/src/Main.java
+++ src/src/Main.java
@@ -13,11 +13,9 @@ public class Main {
 
         // ===== UC2 =====
         List<String> passengerBogies = new ArrayList<>();
-
         passengerBogies.add("Sleeper");
         passengerBogies.add("AC Chair");
         passengerBogies.add("First Class");
-
         passengerBogies.remove("AC Chair");
 
         System.out.println("\nFinal Passenger Bogie List:");
@@ -28,14 +26,13 @@ public class Main {
         bogieIds.add("BG101");
         bogieIds.add("BG102");
         bogieIds.add("BG103");
-        bogieIds.add("BG101"); // duplicate
+        bogieIds.add("BG101");
 
         System.out.println("\nBogie IDs:");
         System.out.println(bogieIds);
 
         // ===== UC4 =====
         LinkedList<String> consist = new LinkedList<>();
-
         consist.add("Engine");
         consist.add("Sleeper");
         consist.add("AC");
@@ -50,17 +47,27 @@ public class Main {
         System.out.println(consist);
 
         // ===== UC5 =====
-        System.out.println("\n=== UC5: Train Formation Using LinkedHashSet ===");
-
         LinkedHashSet<String> formation = new LinkedHashSet<>();
-
         formation.add("Engine");
         formation.add("Sleeper");
         formation.add("Cargo");
         formation.add("Guard");
-        formation.add("Sleeper"); // duplicate
+        formation.add("Sleeper");
 
         System.out.println("\nFinal Train Formation (Insertion Order Preserved, No Duplicates):");
         System.out.println(formation);
+
+        // ===== UC6 =====
+        System.out.println("\n=== UC6: Bogie Capacity Mapping Using HashMap ===");
+
+        HashMap<String, Integer> bogieCapacity = new HashMap<>();
+        bogieCapacity.put("Sleeper", 72);
+        bogieCapacity.put("AC Chair", 60);
+        bogieCapacity.put("First Class", 24);
+
+        System.out.println("\nBogie Capacity Details:");
+        for (Map.Entry<String, Integer> entry : bogieCapacity.entrySet()) {
+            System.out.println(entry.getKey() + " -> Capacity: " + entry.getValue());
+        }
     }
 }
